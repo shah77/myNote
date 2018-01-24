735c641d1ee67f3ee2677d63e4bde75eddd1ada2
haha7 <haha7@gmail.com>
2018-01-24 11:53:20 +0800

    mynote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatCardModule } from '@angular/material';
 
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { QuicknotesComponent } from './quicknotes/quicknotes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+QuicknotesComponent
+
+],
+imports: [
+BrowserModule,
+MatButtonModule,
+MatCheckboxModule,
+MatFormFieldModule,
+MatCardModule,
+MatInputModule,
+FormsModule
+
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
